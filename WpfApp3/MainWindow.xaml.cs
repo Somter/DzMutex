6ac4301bfe5841506b08383f3db6dccaa1cfdd8c
Sasha <alecsand.unit@gmail.com>
2025-02-13 10:19:08 +0200

    hw2

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace WpfApp3
 {
@@ -12,14 +9,14 @@ namespace WpfApp3
     {
         private static Semaphore semaphore = new Semaphore(3, 3, "GlobalAppSemaphore");
         public SynchronizationContext uiContext;
-        Random random = new Random();
-        Mutex mutex = new Mutex();
+        private Random random = new Random();
+        private Mutex mutex = new Mutex();
 
         public MainWindow()
         {
             if (!semaphore.WaitOne(0))
             {
-                MessageBox.Show("Программа не может запустить более 3 потоков.");   
+                MessageBox.Show("Программа не может запустить более 3 потоков.");
                 Close();
                 return;
             }
@@ -31,9 +28,12 @@ namespace WpfApp3
 
         void ThreadFunction1()
         {
-            int[] numbers = new int[100];
-
             mutex.WaitOne();
+            new Thread(ThreadFunction2).Start();
+
+            uiContext.Send(d => txtStatus1.Text = "Поток 1: Запущен", null);
+
+            int[] numbers = new int[100];
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = random.Next(i, 100);
@@ -41,81 +41,63 @@ namespace WpfApp3
 
             File.WriteAllLines("number.txt", numbers.Select(n => n.ToString()));
             Thread.Sleep(1000);
-            uiContext.Send(d => txtStatus1.Text = "Поток 1: Поток записал числа в файл", null); 
-            ThreadFunction2();  
-            mutex.ReleaseMutex();
+            uiContext.Send(d => txtStatus1.Text = "Поток 1: Записал числа в файл", null);
 
-           
+            mutex.ReleaseMutex();
         }
 
         void ThreadFunction2()
         {
-            try
+            mutex.WaitOne();
+            new Thread(ThreadFunction3).Start();
+
+            uiContext.Send(d => txtStatus2.Text = "Поток 2: Запущен", null);
+
+            Thread.Sleep(1000);
+
+            if (!File.Exists("number.txt"))
             {
-                mutex.WaitOne();
-                Thread.Sleep(1000);
-                uiContext.Send(d => txtStatus2.Text = "Поток 2: Мьютекс свободен! Начинаем поиск простых чисел из первого файла.", null);
-
-                if (!File.Exists("number.txt"))
-                {
-                    uiContext.Send(d => txtStatus2.Text = "Поток 2: Файл с числами не найден!", null);
-                    mutex.ReleaseMutex();
-                    return;
-                }
-
-                string[] lines = File.ReadAllLines("number.txt");
-                int[] numbers = lines.Select(line => int.Parse(line)).ToArray();
-
-                var primeNumbers = numbers.Where(IsPrime).ToArray();
-
-                File.WriteAllLines("primeNumbers.txt", primeNumbers.Select(n => n.ToString()));
-
-                Thread.Sleep(1000);
-                uiContext.Send(d => txtStatus2.Text = "Поток 2: Файл с простыми числами создан.", null);
-
-                ThreadFunction3();
+                uiContext.Send(d => txtStatus2.Text = "Поток 2: Файл с числами не найден!", null);
                 mutex.ReleaseMutex();
+                return;
+            }
 
-                
-            }
-            catch (Exception ex)
-            {
-                uiContext.Send(d => txtStatus2.Text = $"Поток 2: Ошибка при обработке: {ex.Message}", null);
-            }
+            string[] lines = File.ReadAllLines("number.txt");
+            int[] numbers = lines.Select(line => int.Parse(line)).ToArray();
+
+            var primeNumbers = numbers.Where(IsPrime).ToArray();
+            File.WriteAllLines("primeNumbers.txt", primeNumbers.Select(n => n.ToString()));
+
+            Thread.Sleep(1000);
+            uiContext.Send(d => txtStatus2.Text = "Поток 2: Файл с простыми числами создан.", null);
+
+            mutex.ReleaseMutex();
         }
 
         void ThreadFunction3()
         {
-            try
+            mutex.WaitOne();
+
+            uiContext.Send(d => txtStatus3.Text = "Поток 3: Запущен", null);
+            Thread.Sleep(1000);
+
+            if (!File.Exists("primeNumbers.txt"))
             {
-                mutex.WaitOne();
-
-                uiContext.Send(d => txtStatus3.Text = "Поток 3: Мьютекс свободен! Начинаем поиск простых чисел с последней цифрой 7.", null);
-                Thread.Sleep(1000);
-
-                if (!File.Exists("primeNumbers.txt"))
-                {
-                    uiContext.Send(d => txtStatus3.Text = "Поток 3: Файл с простыми числами не найден!", null);
-                    mutex.ReleaseMutex();
-                    return;
-                }
-
-                string[] lines = File.ReadAllLines("primeNumbers.txt");
-                int[] primeNumbers = lines.Select(line => int.Parse(line)).ToArray();
-
-                var numbersEndingIn7 = primeNumbers.Where(n => n % 10 == 7).ToArray();
-
-                File.WriteAllLines("primeNumbersEndingIn7.txt", numbersEndingIn7.Select(n => n.ToString()));
-
-                Thread.Sleep(1000);
-                uiContext.Send(d => txtStatus3.Text = "Поток 3: Файл с простыми числами, заканчивающимися на 7, создан.", null);
-
+                uiContext.Send(d => txtStatus3.Text = "Поток 3: Файл с простыми числами не найден!", null);
                 mutex.ReleaseMutex();
+                return;
             }
-            catch (Exception ex)
-            {
-                uiContext.Send(d => txtStatus3.Text = $"Поток 3: Ошибка при обработке: {ex.Message}", null);
-            }
+
+            string[] lines = File.ReadAllLines("primeNumbers.txt");
+            int[] primeNumbers = lines.Select(line => int.Parse(line)).ToArray();
+
+            var numbersEndingIn7 = primeNumbers.Where(n => n % 10 == 7).ToArray();
+            File.WriteAllLines("primeNumbersEndingIn7.txt", numbersEndingIn7.Select(n => n.ToString()));
+
+            Thread.Sleep(1000);
+            uiContext.Send(d => txtStatus3.Text = "Поток 3: Файл с простыми числами, заканчивающимися на 7, создан.", null);
+
+            mutex.ReleaseMutex();
         }
 
         private bool IsPrime(int number)
@@ -136,7 +118,11 @@ namespace WpfApp3
         {
             try
             {
-                Task.Factory.StartNew(ThreadFunction1);
+                txtStatus1.Text = "Поток 1: Ожидает";
+                txtStatus2.Text = "Поток 2: Ожидает";
+                txtStatus3.Text = "Поток 3: Ожидает";
+
+                new Thread(ThreadFunction1).Start();
             }
             catch (Exception ex)
             {
